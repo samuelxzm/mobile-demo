a132c286f2cd2b23416e33bafbcd903b82c1f110
samuelxzm <38691349+samuelxzm@users.noreply.github.com>
2020-03-13 15:34:47 +0800

    初始化

---
+++ Vue.config.js
@@ -0,0 +1,63 @@
+// vue.config.js
+'use strict'
+const path = require('path')
+// const defaultSettings = require('./src/settings.js')
+
+function resolve(dir) {
+    return path.join(__dirname, dir)
+}
+
+const name = '移动端' // page title
+
+// If your port is set to 80,
+// use administrator privileges to execute the command line.
+// For example, Mac: sudo npm run
+// You can change the port by the following method:
+// port = 9527 npm run dev OR npm run dev --port = 9527
+const port = process.env.port || process.env.npm_config_port || 9527 // dev port
+
+module.exports = {
+    // 将 pages 目录添加为新的页面
+    // pages: {
+    //     index: {
+    //         // page 的入口
+    //         entry: 'pages/main.js',
+    //         // 模板来源
+    //         template: 'public/index.html',
+    //         // 输出文件名
+    //         filename: 'index.html'
+    //     }
+    // },
+    chainWebpack: config => {
+        config.module.rules.delete("svg"); //重点:删除默认配置中处理svg,
+        //const svgRule = config.module.rule('svg')
+        //svgRule.uses.clear()
+        config.module
+            .rule('svg-sprite-loader')
+            .test(/\.svg$/)
+            .include
+            .add(resolve('src/assets/icons')) //处理svg目录
+            .end()
+            .use('svg-sprite-loader')
+            .loader('svg-sprite-loader')
+            .options({
+                symbolId: 'icon-[name]'
+            })
+    },
+    configureWebpack: {
+        // provide the app's title in webpack's name field, so that
+        // it can be accessed in index.html to inject the correct title.
+        name: name,
+        resolve: {
+            alias: {
+                '@': resolve('src')
+            }
+        }
+    },
+    devServer: {
+        clientLogLevel: 'warning',
+        historyApiFallback: true,
+        hot: false,
+        inline: false,
+    },
+}
