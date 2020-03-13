d76d389a5768c7b5b827a7ead7acb81407140acc
samuelxzm <38691349+samuelxzm@users.noreply.github.com>
2020-03-13 15:49:44 +0800

    添加实例vant
    添加实例vant

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <van-notice-bar  :scrollable="true" text="通知内容sdfsssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss" left-icon="volume-o" />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import { NoticeBar } from 'vant';
 export default {
+  components:{
+    VanNoticeBar:NoticeBar
+  },
   name: 'Home',
-  components: {
-    HelloWorld
-  }
 }
 </script>
